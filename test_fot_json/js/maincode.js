093a9d68ac543409f2420dce573867f7c7bc3f38
shawim <46514154+shawim@users.noreply.github.com>
2019-03-05 16:16:58 +0800

    Update maincode.js

--- test_fot_json/js/maincode.js
+++ test_fot_json/js/maincode.js
@@ -1,12 +1,24 @@
-var images = [];
-$.ajax({
-    url: 'https://shawim.github.io/test/test_fot_json/gallery_info.json',
-    dataType: 'json',
-    success: function (data) {
-        console.log(data);
-        window.images = data;
-        console.log(images);
-    }
-});
-console.log(images);
+function getImageInfo {
+    var images = [];
+    $.ajax({
+        url: 'https://shawim.github.io/test/test_fot_json/gallery_info.json',
+        dataType: 'json',
+        success: function (data) {
+            if(data)
+            {
+                $.each(data, function(i, item){
+                    var inner = 
+                    {
+                        name: item.name,
+                        title: item.title,
+                        content: item.content,
+                    }
+                    images.push(inner);
+                });
+            }
+        }
+    });
+    return images; 
+    console.log(images);
+}
 
