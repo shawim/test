c444ae0b20b683a6a3d2864630c3e035147e5cec
shawim <46514154+shawim@users.noreply.github.com>
2019-03-05 15:49:03 +0800

    Update maincode.js

--- test_fot_json/js/maincode.js
+++ test_fot_json/js/maincode.js
@@ -3,7 +3,9 @@ $.ajax({
     url: 'https://shawim.github.io/test/test_fot_json/gallery_info.json',
     dataType: 'json',
     success: function (data) {
-        images = data;
+        console.log(data);
+        this.images = data;
     }
 });
 console.log(images);
+
